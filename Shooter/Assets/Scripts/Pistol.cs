ff788bdb2c64b11a53e34864f2aa5f56e9a09c4a
Rauder999 <121938845+Rauder999@users.noreply.github.com>
2023-05-18 22:20:41 +0200

    Correct DamageUI

--- Shooter/Assets/Scripts/Pistol.cs
+++ Shooter/Assets/Scripts/Pistol.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Pistol : Weapon
 {
+    [SerializeField] private float shootInterval;
+    private float shootTimer;
+    private bool canShoot = true;
+
+    protected override void Update()
+    {
+        base.Update();
+        if (!canShoot)
+        {
+            shootTimer += Time.deltaTime;
+            if (shootTimer >= shootInterval)
+            {
+                canShoot = true;
+                shootTimer = 0f;
+            }
+        }
+    }
     public override void Shoot()
     {
-        Instantiate(bullet, shotPoint.position, shotPoint.rotation);
-        shootEffect.Play();
+        if (canShoot)
+        {
+            Instantiate(bullet, shotPoint.position, shotPoint.rotation);
+            shootEffect.Play();
+
+            canShoot = false;
+            shootTimer = 0;
+        }
+        
     }
 
     public override void OnInit()
