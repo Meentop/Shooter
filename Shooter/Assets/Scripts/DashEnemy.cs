53aa5982e97e3f0efedb945a52d7b5c7531e846b
Meentop <meentrop@gmail.com>
2023-05-19 12:07:54 +0300

    Add ready dash enemy

--- Shooter/Assets/Scripts/DashEnemy.cs
+++ Shooter/Assets/Scripts/DashEnemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DashEnemy : Enemy
 {
     [SerializeField] private float attackRange;
+    private bool _isDashing;
 
     protected override void Update()
     {
@@ -16,35 +17,8 @@ public class DashEnemy : Enemy
         }
     }
 
-    private IEnumerator Attack()
-    {
-        print("start attacking");
-        isAttacking = true;
-        anim.SetTrigger("Attack");
-        agent.enabled = false;
-
-        yield return new WaitForSeconds(1.05f);
-
-        print("start dash");
-        rb.AddForce(transform.forward * 20f, ForceMode.Impulse);
-
-        yield return new WaitForSeconds(0.1f);
-
-        rb.velocity = Vector3.zero; 
-        print("end dash");
-
-        yield return new WaitForSeconds(1.05f);
-
-        anim.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        anim.transform.localPosition = Vector3.zero;
-        agent.enabled = true;
-        isAttacking = false;
-        print("end attacking");
-    }
-
     public void StartAttack()
-    {
-        
+    {      
         agent.enabled = false;
     }
 
@@ -62,21 +36,5 @@ public class DashEnemy : Enemy
     {    
         agent.enabled = true;
         isAttacking = false;
-    }
-
-    public void ResetTransform()
-    {
-        //StartCoroutine(ResetTr());
-    }
-
-    IEnumerator ResetTr()
-    {
-        anim.applyRootMotion = false;
-        anim.transform.localRotation = Quaternion.Euler(Vector3.zero);
-        anim.transform.localPosition = Vector3.zero;
-        print(anim.transform.localRotation.eulerAngles);
-        yield return new WaitForEndOfFrame();
-        anim.applyRootMotion = false;
-        print(anim.transform.localRotation.eulerAngles);
     }
 }
