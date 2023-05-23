ea32c012a9499f1d19eceaf16d2a4fad8860fa5f
Meentop <meentrop@gmail.com>
2023-05-23 21:33:17 +0300

    Shotgun refactor

--- Shooter/Assets/Scripts/Weapons/Shotgun.cs
+++ Shooter/Assets/Scripts/Weapons/Shotgun.cs
@@ -27,25 +27,30 @@ public class Shotgun : Weapon
     public override void Shoot()
     {
         if(canShoot)
-        {     
-        for (int i = 0; i < pelletsCount; i++)
         {
-            Vector3 spreadDirection = Quaternion.Euler(Random.Range(-spreadAngle, spreadAngle), Random.Range(-spreadAngle, spreadAngle), 0) * Camera.main.transform.forward;
-            RaycastHit hit;
-            if (Physics.Raycast(Camera.main.transform.position, spreadDirection, out hit, range))
+            for (int i = 0; i < pelletsCount; i++)
             {
-                if (hit.transform.TryGetComponent<IDamageReceiver>(out var damageReceiver))
+                Vector3 direction = Camera.main.transform.forward; 
+                Vector3 spread = Vector3.zero;
+                spread += Camera.main.transform.up * Random.Range(-1f, 1f); 
+                spread += Camera.main.transform.right * Random.Range(-1f, 1f); 
+                direction += spread.normalized * Random.Range(0f, 0.1f);
+
+                RaycastHit hit;
+                if (Physics.Raycast(Camera.main.transform.position, direction, out hit, range))
                 {
-                    damageReceiver.OnGetDamage(new DamageData(Random.Range(damageRange.x, damageRange.y), hit));
-                }
-                else
-                {
-                    var decal = Instantiate(decalPrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
-                    decal.transform.SetParent(hit.transform);
-                    Destroy(decal, 5);
+                    if (hit.transform.TryGetComponent<IDamageReceiver>(out var damageReceiver))
+                    {
+                        damageReceiver.OnGetDamage(new DamageData(Random.Range(damageRange.x, damageRange.y), hit));
+                    }
+                    else
+                    {
+                        var decal = Instantiate(decalPrefab, hit.point + hit.normal * 0.001f, Quaternion.LookRotation(hit.normal));
+                        decal.transform.SetParent(hit.transform);
+                        Destroy(decal, 5);
+                    }
                 }
             }
-        }
             shootEffect.Play();
             canShoot = false;
             shootTimer = 0;
