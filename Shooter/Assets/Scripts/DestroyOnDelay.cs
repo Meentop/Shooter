645ac31756ad49a757f6edd5ab6ea8d91d613683
Meentop <meentrop@gmail.com>
2023-05-24 15:25:44 +0300

    Change fireboll

--- Shooter/Assets/Scripts/DestroyOnDelay.cs
+++ Shooter/Assets/Scripts/DestroyOnDelay.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DestroyOnDelay : MonoBehaviour
 {
     [SerializeField] private float delayToDestroy;
+
     private void Start()
     {
         Destroy(gameObject, delayToDestroy);
