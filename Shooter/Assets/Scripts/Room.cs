509cc84c0ce34fcc93bb4afab19eda87144ff41c
Rauder999 <121938845+Rauder999@users.noreply.github.com>
2023-06-05 10:24:54 +0200

    Refactor

--- Shooter/Assets/Scripts/Room.cs
+++ Shooter/Assets/Scripts/Room.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System;
 using UnityEngine;
 
@@ -8,10 +6,9 @@ public class Room : MonoBehaviour
     public Vector2Int place;
 
     [SerializeField] private GameObject[] doors;
-    Vector2Int[] directions = { new Vector2Int(0, 1), new Vector2Int(1, 0), new Vector2Int(0, -1), new Vector2Int(-1, 0) };
-
+    
     public void SetOpenDoor(Vector2Int direction)
     {
-        doors[Array.IndexOf(directions, direction)].SetActive(false);
+        doors[Array.IndexOf(RoomSpawner.directions, direction)].SetActive(false);
     }
 }
